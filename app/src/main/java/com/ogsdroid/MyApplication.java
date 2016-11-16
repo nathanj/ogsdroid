379d522d99762ec20cfc1ddc6a1f0632b7bb75d9
Nathan Jones <nathanj439@gmail.com>
2016-11-15 22:35:43 -0500

    Add ACRA to report crashes

---
+++ app/src/main/java/com/ogsdroid/MyApplication.java
@@ -0,0 +1,24 @@
+package com.ogsdroid;
+
+import android.app.Application;
+import android.content.Context;
+
+import org.acra.*;
+import org.acra.annotation.ReportsCrashes;
+
+@ReportsCrashes(
+        mailTo = "nathanj439@gmail.com",
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.crash_toast_text
+)
+
+public class MyApplication extends Application {
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
+
+}
