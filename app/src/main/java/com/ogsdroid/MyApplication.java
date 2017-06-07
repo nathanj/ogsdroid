6140c9cd0315cdd6e7a9bc824f774320f73717f5
Nathan Jones <nathanj439@gmail.com>
2017-06-06 21:11:37 -0400

    Include logcat in crash reports

--- app/src/main/java/com/ogsdroid/MyApplication.java
+++ app/src/main/java/com/ogsdroid/MyApplication.java
@@ -4,12 +4,14 @@ import android.app.Application;
 import android.content.Context;
 
 import org.acra.ACRA;
+import org.acra.ReportField;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
 @ReportsCrashes(
         mailTo = "nathanj439@gmail.com",
         mode = ReportingInteractionMode.TOAST,
+        customReportContent = {ReportField.APP_VERSION_CODE, ReportField.APP_VERSION_NAME, ReportField.ANDROID_VERSION, ReportField.PHONE_MODEL, ReportField.STACK_TRACE, ReportField.LOGCAT },
         resToastText = R.string.crash_toast_text
 )
 
